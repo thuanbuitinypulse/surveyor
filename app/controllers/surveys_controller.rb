3805fd33b361d1f1e96a4960a631924a7c2c9d65
Harley Trung <harley@bsa.edu.vn>
2016-07-19 23:32:18 +0700

    edit survey view

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -12,13 +12,30 @@ class SurveysController < ApplicationController
     end
   end
 
+  def edit
+    load_survey
+  end
+
+  def update
+    load_survey
+    if @survey.update_attributes survey_params
+      redirect_to edit_survey_path, notice: "Saved."
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def survey_params
-    params.require(:survey).permit(:title)
+    params.require(:survey).permit(:title, :questions)
   end
 
   def build_survey
     @survey = Survey.new
+  end
+
+  def load_survey
+    @survey = Survey.find params[:id]
   end
 end
